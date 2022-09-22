83652a3d693a5c9f04f9605ec6641f7f630937e9
Igor Pogorelov <pogoreelov@yandex.ru>
2022-09-22 22:12:05 +0500

    Int to Long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager manager = new SalesManager(new int[] {1,2,3,4,5,6,7,8,9});
+        SalesManager manager = new SalesManager(new long[] {1,2,3,4,5,6,7,8,9});
         System.out.println("Самая большая продажа: " + manager.max());
     }
 }
